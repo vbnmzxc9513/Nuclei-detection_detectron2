4beb4f327311a934b93c8ea955a44064c98733fa
ndhu410421304 <33059129+ndhu410421304@users.noreply.github.com>
2020-01-02 02:43:40 +0800

    Update with style

--- train_Cascade.py
+++ train_Cascade.py
@@ -1,14 +1,11 @@
 import detectron2
 from detectron2.utils.logger import setup_logger
-setup_logger()
-
 # import some common libraries
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import random
 import os
-
 # import some common detectron2 utilities
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
@@ -18,6 +15,8 @@ from detectron2.data.datasets import register_coco_instances
 from detectron2.engine import DefaultTrainer
 from detectron2.config import get_cfg
 
+setup_logger()
+
 register_coco_instances("my_dataset", {}, "nucleus_cocoformat.json", "./train")
 metadata = MetadataCatalog.get("my_dataset")
 dataset_dicts = DatasetCatalog.get("my_dataset")
@@ -25,17 +24,20 @@ dataset_dicts = DatasetCatalog.get("my_dataset")
 cfg = get_cfg()
 cfg.merge_from_file("./model/mask_rcnn_R_50_FPN_3x.yaml")
 cfg.OUTPUT_DIR = "./output"
-#cfg.MODEL.WEIGHTS = os.path.join('model', "model_final_Cascade.pkl")  # if you have pre-trained weight.
+# cfg.MODEL.WEIGHTS = os.path.join('model', "model_final_Cascade.pkl")
+# if you have pre-trained weight.
 cfg.DATASETS.TRAIN = ("my_dataset",)
 cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.00025
-cfg.SOLVER.MAX_ITER = 300000    # 300 iterations seems good enough, but you can certainly train longer
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset
+# 4999 iterations seems good enough, but you can certainly train longer
+cfg.SOLVER.MAX_ITER = 300000
+# faster, and good enough for this toy dataset
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # 1 classes (data, fig, hazelnut)
 
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True) #  build output folder
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)  # build output folder
 trainer = DefaultTrainer(cfg)
 trainer.resume_or_load(resume=False)
 trainer.train()
